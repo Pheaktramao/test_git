8269270ca8eb9a3cf4e5ec35c7a4d309b1b9cc50
PHAL.HIM <PHAL.HIM@PNC.LAN>
2024-02-08 16:36:44 +0700

    [feat] create table TEST-4

---
+++ listCategory/list.Category.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <table class="table">
+            <thead class="">
+                <tr>
+                    <td>ID</td>
+                    <td>FistName</td>
+                    <td>LastName</td>
+                    <td>Email</td>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
